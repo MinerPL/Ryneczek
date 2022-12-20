bef5e62173585d4f8c9e6df8575b3c8b4ccbd38d
MinerPL <minerpl03@gmail.com>
2022-12-20 20:23:20 +0100

    feat: Move events to sub folders

--- src/classes/Handlers/EventHandler.ts
+++ src/classes/Handlers/EventHandler.ts
@@ -8,16 +8,23 @@ export default class EventHandler {
 	}
 
 	async loadEvents() {
-		const files = readdirSync(`${__dirname}/../../events`);
+		let i = 0;
+		const dirs = readdirSync(`${__dirname}/../../events`);
 
-		for (const rawFile of files) {
-			const file = await import(`${__dirname}/../../events/${rawFile}`);
+		for (const dir of dirs) {
+			const files = readdirSync(`${__dirname}/../../events/${dir}`);
 
-			this.client.on(rawFile.slice(0, -3), (...event) => {
-				file.run(this.client, ...event);
-			});
+			for(const rawFile of files) {
+				if (!rawFile.endsWith('.js')) continue;
+				const file = await import(`${__dirname}/../../events/${dir}/${rawFile}`);
+
+				this.client.on(rawFile.slice(0, -3), (...event) => {
+					file.run(this.client, ...event);
+				});
+				i++;
+			}
 		}
 
-		console.log(`${files.length} events loaded.`);
+		console.log(`${i} events loaded.`);
 	}
 }
