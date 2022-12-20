d2c7dfbf019883f0c35c394ea66e89e8dc17637a
MinerPL <minerpl03@gmail.com>
2022-12-20 19:59:28 +0100

    chore: Remove unnecessary requires

--- src/commands/admin/deploy.ts
+++ src/commands/admin/deploy.ts
@@ -1,6 +1,5 @@
 import { CommandInteraction, GuildMember, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
 import { Routes } from 'discord-api-types/v10';
-import { REST } from '@discordjs/rest';
 import Ryneczek from '@classes/Ryneczek';
 
 export const data = {
@@ -17,9 +16,7 @@ export async function run(client: Ryneczek, interaction: CommandInteraction) {
 	if(!(interaction.member as GuildMember).roles.cache.has('811550637518487563')) return interaction.reply({ content: 'Nie masz uprawnień do tej komendy!', ephemeral: true });
 	await interaction.deferReply();
 
-	const rest = new REST({ version: '10' }).setToken(client.config.token);
-
-	await rest.put(
+	await client.rest.put(
 		Routes.applicationGuildCommands(client.user.id, '811550188823904277'),
 
 		{ body: [...client.commands.map(x => x.data), ...client.commands.filter(x => x.data.context).map(x => x.data.context)] },
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,5 @@
 import Ryneczek from '@classes/Ryneczek';
 import { ActivityType, Routes } from 'discord-api-types/v10';
-import { REST } from '@discordjs/rest';
 
 export async function run(client: Ryneczek) {
 	console.log(`${client.user.tag} is ready!`);
@@ -10,9 +9,7 @@ export async function run(client: Ryneczek) {
 	});
 
 	if(process.argv.includes('--deploy')) {
-		const rest = new REST({ version: '10' }).setToken(client.config.token);
-
-		await rest.put(
+		await client.rest.put(
 			Routes.applicationGuildCommands(client.user.id, '811550188823904277'),
 
 			{ body: [...client.commands.map(x => x.data), ...client.commands.filter(x => x.data.context).map(x => x.data.context)] },
