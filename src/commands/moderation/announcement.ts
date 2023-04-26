ade0127a09a9e7cb99c70c04eef487e0c2f86d39
Dawid Jaworski <contact@minerpl.xyz>
2023-04-26 23:27:13 +0200

    feat: Create simple announcement command

---
+++ src/commands/moderation/announcement.ts
@@ -0,0 +1,60 @@
+import {
+	BaseGuildTextChannel,
+	ChannelType,
+	ChatInputCommandInteraction,
+	ColorResolvable,
+	EmbedBuilder,
+	PermissionFlagsBits,
+	SlashCommandBuilder,
+} from 'discord.js';
+import Ryneczek from '@classes/Ryneczek';
+
+export const data = {
+	...new SlashCommandBuilder()
+		.setName('announcement')
+		.setDescription('Wysyła ogłoszenie na wybrany kanał.')
+		.setDMPermission(false)
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+		.addChannelOption(option =>
+			option.setName('kanał')
+				.setDescription('Kanał, na który ma zostać wysłane ogłoszenie.')
+				.addChannelTypes(ChannelType.GuildAnnouncement, ChannelType.GuildText)
+				.setRequired(true),
+		)
+		.addStringOption(option =>
+			option.setName('tytuł')
+				.setDescription('Tytuł ogłoszenia.')
+				.setRequired(true),
+		)
+		.addStringOption(option =>
+			option.setName('treść')
+				.setDescription('Treść ogłoszenia.')
+				.setRequired(true),
+		)
+		.addStringOption(option =>
+			option.setName('kolor')
+				.setDescription('Kolor ogłoszenia.')
+				.setRequired(false),
+		)
+		.toJSON(),
+};
+
+export async function run(client: Ryneczek, interaction: ChatInputCommandInteraction) {
+	const channel = interaction.options.getChannel('kanał') as BaseGuildTextChannel;
+	const title = interaction.options.getString('tytuł');
+	const content = interaction.options.getString('treść');
+	const color = (interaction.options.getString('kolor') || '#87b55b') as ColorResolvable;
+
+	const embed = new EmbedBuilder()
+		.setTitle(title)
+		.setDescription(content)
+		.setColor(color)
+		.setFooter({
+			text: interaction.guild.name,
+			iconURL: interaction.guild.iconURL(),
+		})
+		.setTimestamp();
+
+	await channel.send({ embeds: [embed] });
+	await interaction.reply({ content: 'Wysłano ogłoszenie.', ephemeral: true });
+}
